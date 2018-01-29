1a42c1c3622490aa810fc090b68bbb9fc96a7124
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-29 21:41:06 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(arr)
+  return arr.sort
+end
 
+def sort_array_desc(arr)
+  return arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.map do |name|
+    name.tr_s(name[2], "$")
+  end
+end
+
+def find_a(arr)
+  arr.select {|obj| obj[0] == "a"}
+end
+
+def sum_array(arr)
+  arr.reduce(:+)
+end
+
+def add_s(arr)
+  arr.map do |word|
+    if word != arr[1]
+      word.insert(-1, "s")
+    else
+      word
+    end
+  end
+end
