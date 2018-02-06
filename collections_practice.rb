46e53b4fe8076578d1f01a08bebb02abe9c59dc1
porzingod <jasonlim.ny@gmail.com>
2018-02-06 10:06:02 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map {|x| x.gsub(x[2], '$')}
+end
+
+def find_a(array)
+  array.select {|x| x[0] == 'a'}
+end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def add_s(array)
+  array.map do |x|
+    if x == array[1]
+      x
+    else
+      x + 's' end
+  end
+end
